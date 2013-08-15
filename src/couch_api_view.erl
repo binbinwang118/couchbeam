6a639e962b20754d368aa55a4928e0d3f434e417
Jing Luo <luojinglp@gmail.com>
2013-08-15 15:30:30 +0800

    extend the couchdb view api  - design_doc_info  - all doc  - fetch view  - show

--- src/couch_api_view.erl
+++ src/couch_api_view.erl
@@ -12,6 +12,9 @@
 %% API functions
 %% ====================================================================
 -export([start_link/0]).
+-export([design_doc_info/2,all/1,all/2,
+		 fetch/1,fetch/2,fetch/3,
+		 show/2]).
 
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -37,6 +40,68 @@ init([]) ->
 	process_flag(trap_exit, true),
     {ok, #state{}}.
 
+%% API
+%% ====================================================================
+%% DDocId:string()
+design_doc_info(Db,DDocId) ->
+	gen_server:call(?MODULE, {design_doc_info, Db, DDocId}, ?ApiTimeout).
+
+-spec all(Db::db()) -> {ok, Rows::list(ejson_object())} | {error, term()}.
+%% @doc fetch all docs
+%% @equiv fetch(Db, 'all_docs', [])
+all(Db) ->
+    fetch(Db, 'all_docs', []).
+
+-spec all(Db::db(), Options::view_options())
+        -> {ok, Rows::list(ejson_object())} | {error, term()}.
+%% @doc fetch all docs
+%% @equiv fetch(Db, 'all_docs', Options)
+all(Db, Options) ->
+    fetch(Db, 'all_docs', Options).
+
+-spec fetch(Db::db()) -> {ok, Rows::list(ejson_object())} | {error, term()}.
+%% @equiv fetch(Db, 'all_docs', [])
+fetch(Db) ->
+    fetch(Db, 'all_docs', []).
+
+-spec fetch(Db::db(), ViewName::'all_docs' | {DesignName::string(),
+        ViewName::string()})
+    -> {ok, Rows::list(ejson_object())} | {error, term()}.
+%% @equiv fetch(Db, ViewName, [])
+fetch(Db, ViewName) ->
+    fetch(Db, ViewName,[]).
+
+-spec fetch(Db::db(), ViewName::'all_docs' | {DesignName::string(),
+        ViewName::string()}, Options::view_options())
+     -> {ok, Rows::list(ejson_object())} | {error, term()}.
+%% @doc Collect view results
+%%  <p>Db: a db record</p>
+%%  <p>ViewName: <code>'all_docs'</code> to get all docs or 
+%%      <code>{DesignName,ViewName}</code></p>
+%%  <pre>Options :: view_options() [{key, binary()} | {start_docid, binary()}
+%%    | {end_docid, binary()} | {start_key, binary()}
+%%    | {end_key, binary()} | {limit, integer()}
+%%    | {stale, stale()}
+%%    | descending
+%%    | {skip, integer()}
+%%    | group | {group_level, integer()}
+%%    | {inclusive_end, boolean()} | {reduce, boolean()} | reduce | include_docs | conflicts
+%%    | {keys, list(binary())}</pre>
+%% <p>See {@link couchbeam_view:stream/4} for more information about
+%% options.</p>
+%% <p>Return: {ok, Rows} or {error, Rows, Error}</p>
+fetch(Db, ViewName, Options) ->
+	gen_server:call(?MODULE, {fetch, Db, ViewName, Options}, ?ApiTimeout).
+	
+
+%% DDocId:string()
+%% Item:string()
+%% Key: string()
+%% example: {DDocId,Item,Key}={couchbeam,detail,biking}
+show(Db,{DDocId,Item,Key}) ->
+	gen_server:call(?MODULE, {show, Db,{DDocId,Item,Key}}, ?ApiTimeout).
+
+%% ====================================================================
 
 %% handle_call/3
 %% ====================================================================
@@ -55,10 +120,14 @@ init([]) ->
 	Timeout :: non_neg_integer() | infinity,
 	Reason :: term().
 %% ====================================================================
-handle_call(Request, From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+handle_call({design_doc_info, Db, DDocId}, _From, State) ->
+    {reply, design_doc_info_internal(Db, DDocId), State};
 
+handle_call({fetch, Db, ViewName, Options}, _From, State) ->
+    {reply, fetch_internal(Db, ViewName, Options), State};
+
+handle_call({show, Db,{DDocId,Item,Key}}, _From, State) ->
+    {reply, show_internal(Db,{DDocId,Item,Key}), State}.
 
 %% handle_cast/2
 %% ====================================================================
@@ -118,5 +187,257 @@ code_change(OldVsn, State, Extra) ->
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+design_doc_info_internal(#db{server=Server, options=IbrowseOpts}=Db,DDocId) ->
+	Url = couchbeam_util:make_url(Server,[couchbeam_util:db_url(Db), "/_design/", DDocId,"/_info"],[]),
+	case couchbeam_httpc:request(get, Url, ["200"], IbrowseOpts) of
+		{ok, _, _, RespBody} ->
+            {ok, couchbeam_ejson:decode(RespBody)};
+        Error ->
+            Error
+    end.
+
+fetch_internal(Db, ViewName, Options) ->
+    case stream(Db, ViewName, self(), Options) of
+        {ok, StartRef, _} ->
+            collect_view_results(StartRef, []);
+        Error ->
+            Error
+    end.
+
+-spec stream(Db::db(), Client::pid()) -> {ok, StartRef::term(),
+        ViewPid::pid()} | {error, term()}.
+%% @equiv stream(Db, 'all_docs', Client, [])
+stream(Db, Client) ->
+    stream(Db, 'all_docs', Client, []).
+
+
+-spec stream(Db::db(), ViewName::'all_docs' | {DesignName::string(),
+        ViewName::string()}, Client::pid()) -> {ok, StartRef::term(),
+        ViewPid::pid()} | {error, term()}.
+%% @equiv stream(Db, ViewName, Client, [])
+stream(Db, ViewName, Client) ->
+    stream(Db, ViewName, Client, []).
+
+-spec stream(Db::db(), ViewName::'all_docs' | {DesignName::string(),
+        ViewName::string()}, Client::pid(), Options::view_options())
+    -> {ok, StartRef::term(), ViewPid::pid()} | {error, term()}.
+%% @doc stream view results to a pid
+%%  <p>Db: a db record</p>
+%%  <p>ViewName: 'all_docs' to get all docs or {DesignName,
+%%  ViewName}</p>
+%%  <p>Client: pid where to send view events where events are:
+%%  <dl>
+%%      <dt>{row, StartRef, done}</dt>
+%%          <dd>All view results have been fetched</dd>
+%%      <dt>{row, StartRef, Row :: ejson_object()}</dt>
+%%          <dd>A row in the view</dd>
+%%      <dt>{error, StartRef, Error}</dt>
+%%          <dd>Got an error, connection is closed when an error
+%%          happend.</dd>
+%%  </dl></p>
+%%  <p><pre>Options :: view_options() [{key, binary()} | {start_docid, binary()}
+%%    | {end_docid, binary()} | {start_key, binary()}
+%%    | {end_key, binary()} | {limit, integer()}
+%%    | {stale, stale()}
+%%    | descending
+%%    | {skip, integer()}
+%%    | group | {group_level, integer()}
+%%    | {inclusive_end, boolean()} | {reduce, boolean()} | reduce | include_docs | conflicts
+%%    | {keys, list(binary())}</pre>
+%%
+%%  <ul>
+%%      <li><code>{key, Key}</code>: key value</li>
+%%      <li><code>{start_docid, DocId}</code>: document id to start with (to allow pagination
+%%          for duplicate start keys</li>
+%%      <li><code>{end_docid, DocId}</code>: last document id to include in the result (to
+%%          allow pagination for duplicate endkeys)</li>
+%%      <li><code>{start_key, Key}</code>: start result from key value</li>
+%%      <li><code>{end_key, Key}</code>: end result from key value</li>
+%%      <li><code>{limit, Limit}</code>: Limit the number of documents in the result</li>
+%%      <li><code>{stale, Stale}</code>: If stale=ok is set, CouchDB will not refresh the view
+%%      even if it is stale, the benefit is a an improved query latency. If
+%%      stale=update_after is set, CouchDB will update the view after the stale
+%%      result is returned.</li>
+%%      <li><code>descending</code>: reverse the result</li>
+%%      <li><code>{skip, N}</code>: skip n number of documents</li>
+%%      <li><code>group</code>: the reduce function reduces to a single result
+%%      row.</li>
+%%      <li><code>{group_level, Level}</code>: the reduce function reduces to a set
+%%      of distinct keys.</li>
+%%      <li><code>{reduce, boolean()}</code>: whether to use the reduce function of the view. It defaults to
+%%      true, if a reduce function is defined and to false otherwise.</li>
+%%      <li><code>include_docs</code>: automatically fetch and include the document
+%%      which emitted each view entry</li>
+%%      <li><code>{inclusive_end, boolean()}</code>: Controls whether the endkey is included in
+%%      the result. It defaults to true.</li>
+%%      <li><code>conflicts</code>: include conflicts</li>
+%%      <li><code>{keys, [Keys]}</code>: to pass multiple keys to the view query</li>
+%%  </ul></p>
+%%
+%% <p> Return <code>{ok, StartRef, ViewPid}</code> or <code>{error,
+%Error}</code>. Ref can be
+%% used to disctint all changes from this pid. ViewPid is the pid of
+%% the view loop process. Can be used to monitor it or kill it
+%% when needed.</p>
+stream(#db{options=IbrowseOpts}=Db, ViewName, ClientPid, Options) ->
+    make_view(Db, ViewName, Options, fun(Args, Url) ->
+        StartRef = make_ref(),
+        UserFun = fun
+            (done) ->
+                ClientPid ! {row, StartRef, done};
+            ({error, Error}) ->
+                ClientPid ! {error, StartRef, Error};
+            (Row) ->
+                ClientPid ! {row, StartRef, Row}
+        end,
+        Params = {Args, Url, IbrowseOpts},
+        ViewPid = spawn_link(couchbeam_view, view_loop, [UserFun, Params]),
+
+        %% if we send multiple keys, we do a Post
+        Result = case Args#view_query_args.method of
+            get ->
+                couchbeam_httpc:request_stream({ViewPid, once}, get, Url, IbrowseOpts);
+            post ->
+                Body = couchbeam_ejson:encode({[{<<"keys">>, Args#view_query_args.keys}]}),
+                Headers = [{"Content-Type", "application/json"}],
+                couchbeam_httpc:request_stream({ViewPid, once}, post, Url,
+                    IbrowseOpts, Headers, Body)
+        end,
+
+        case Result of
+            {ok, ReqId} ->
+                ViewPid ! {ibrowse_req_id, ReqId},
+                {ok, StartRef, ViewPid};
+            Error ->
+                Error
+        end
+    end).
+
+collect_view_results(Ref, Acc) ->
+    receive
+        {row, Ref, done} ->
+            Rows = lists:reverse(Acc),
+            {ok, Rows};
+        {row, Ref, Row} ->
+            collect_view_results(Ref, [Row|Acc]);
+        {error, Ref, Error} ->
+            %% in case we got some results
+            Rows = lists:reverse(Acc),
+            {error, Rows, Error}
+    end.
+
+make_view(#db{server=Server}=Db, ViewName, Options, Fun) ->
+    Args = parse_view_options(Options),
+    case ViewName of
+        'all_docs' ->
+            Url = couchbeam:make_url(Server, [couchbeam:db_url(Db),
+                    "/_all_docs"],
+                    Args#view_query_args.options),
+            Fun(Args, Url);
+        {DName, VName} ->
+            Url = couchbeam:make_url(Server, [couchbeam:db_url(Db),
+                    "/_design/", DName, "/_view/", VName],
+                    Args#view_query_args.options),
+            Fun(Args, Url);
+        _ ->
+            {error, invalid_view_name}
+    end.
+
+-spec parse_view_options(Options::list()) -> view_query_args().
+%% @doc parse view options
+parse_view_options(Options) ->
+    parse_view_options(Options, #view_query_args{}).
+
+parse_view_options([], Args) ->
+    Args;
+parse_view_options([{key, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{key, couchbeam_ejson:encode(Value)}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{start_docid, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{start_docid, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{end_docid, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{end_docid, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{start_key, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{start_key, couchbeam_ejson:encode(Value)}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{end_key, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{end_key, couchbeam_ejson:encode(Value)}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{startkey, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{startkey, couchbeam_ejson:encode(Value)}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{endkey, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{endkey, couchbeam_ejson:encode(Value)}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{limit, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{limit, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{stale, ok}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{stale, "ok"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{stale, update_after}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{stale, "update_after"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{stale, _}|_Rest], _Args) ->
+    {error, "invalid stale value"};
+parse_view_options([descending|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{descending, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([group|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{group, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{group_level, Level}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{group_level, Level}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([inclusive_end|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{inclusive_end, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{inclusive_end, true}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{inclusive_end, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{inclusive_end, false}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{inclusive_end, "false"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([reduce|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{reduce, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{reduce, true}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{reduce, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{reduce, false}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{reduce, "false"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([include_docs|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{include_docs, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([conflicts|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{conflicts, "true"}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{skip, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{skip, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{list, Value}|Rest], #view_query_args{options=Opts}=Args) ->
+    Opts1 = [{list, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([{keys, Value}|Rest], Args) ->
+    parse_view_options(Rest, Args#view_query_args{method=post,
+            keys=Value});
+parse_view_options([{Key, Value}|Rest], #view_query_args{options=Opts}=Args)
+        when is_list(Key) ->
+    Opts1 = [{Key, Value}|Opts],
+    parse_view_options(Rest, Args#view_query_args{options=Opts1});
+parse_view_options([_|Rest], Args) ->
+    parse_view_options(Rest, Args).
+
+show_internal(#db{server=Server, options=IbrowseOpts}=Db,{DDocId,Item,Key}) ->
+	Url = couchbeam_util:make_url(Server,[couchbeam_util:db_url(Db), "/_design/", DDocId,"/_show/",Item,"/",Key],[]),
+	case couchbeam_httpc:request(get, Url, ["200"], IbrowseOpts) of
+		{ok, _, _, RespBody} ->
+            {ok, RespBody};
+        Error ->
+            Error
+    end.
 
 
